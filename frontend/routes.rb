fac6dcb73db66d5668a040cf05826ef3013582c0
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-09 15:45:59 +0800

    Delete frontend directory

--- frontend/routes.rb
+++
@@ -1,9 +0,0 @@
-ArchivesSpace::Application.routes.draw do
-
-  [AppConfig[:frontend_proxy_prefix], AppConfig[:frontend_prefix]].uniq.each do |prefix|
-
-    scope prefix do
-      match('/plugins/bulk_create_containers/create' => 'bulk_create_containers#create', :via => [:get])
-    end
-  end
-end
